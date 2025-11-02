f1b7b3ef31d1deb26787aedd57e6978e0af09510
Nick Talbert <nickjamestalbert@gmail.com>
2025-11-01 20:43:00 -0700

    user interface updates, .gitignore and __init__.py update

--- __init__.py
+++ __init__.py
@@ -1,2 +1,4 @@
+from . import motor_control
+from . import star_track
 from . import track_object
-
+from . import util
--- star_track.py
+++ star_track.py
@@ -1,35 +1,48 @@
 #!/usr/bin/python3
 """
 Control servos to track celestial objects.
+
+This is the command line interface.
 """
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 import sys
-import time
+from typing import Tuple
 
 from .track_object import begin_track
 
-# import RPi.GPIO as GPIO
-
-# GPIO.setmode(GPIO.BOARD)
-
-# GPIO.setup(7,GPIO.OUT)
-# GPIO.setup(11,GPIO.OUT)
-
-# GPIO.cleanup()
-
-def parse_args() -> None:
-    parser = ArgumentParser()
-    parser.add_argument(
+def parse_args() -> Namespace:
+    parser = ArgumentParser(prog='star_track')
+    mutually_exclusive_args = parser.add_mutually_exclusive_group(required=True)
+    mutually_exclusive_args.add_argument(
+        '-n', '--name',
+        dest='name',
+        metavar='NAME',
         type=str,
+        help='Name of the celestial object to track (e.g., "Mars", "Polaris")',
     )
-    return parser.parse_args()
+    mutually_exclusive_args.add_argument(
+        '-c', '--coords',
+        dest='coords',
+        metavar=('LAT', 'LON'),
+        nargs=2,
+        type=float,
+        help='Pair of coordinates (latitude longitude) to track',
+    )
+    args = parser.parse_args()
+    
+    return args
+
+def get_coordinates(name: str) -> Tuple[float, float]:
+    return (0.0, 0.0)
 
 def main():
     args = parse_args()
-    begin_track(args.obj)
-
-    return 0
+    if args.name:
+        # convert name to a pair of coordinates
+        right_ascension, declination = get_coordinates(args.name)
+    else:
+        right_ascension, declination = args.coords
+    return begin_track(right_ascension, declination)
 
 if __name__ == '__main__':
     sys.exit(main())
-
